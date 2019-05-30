717bba3caba9661f2947d18a75ad33ede820e16b
Joshua Tuscan <hello@joshuatuscan.com>
2019-05-30 20:42:34 +0200

    writing a bunch of javascript to demonstrate

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,7 @@ function showMessage() {
   console.log("message button clicked");
 
   overlay.classList.add("show");
+  overlay.classList.add("newstyle");
 }
 
 function hideOverlay() {
@@ -11,23 +12,6 @@ function hideOverlay() {
   overlay.classList.remove("show");
 }
 
-window.onload = function() {
-  console.log("page loaded!");
-
-  let message_btn = document.querySelector(".message_btn");
-  let overlay = document.querySelector("#overlay");
-
-  overlay.onclick = function(e) {
-    e.preventDefault();
-    hideOverlay();
-  };
-
-  message_btn.onclick = function(e) {
-    e.preventDefault();
-    showMessage();
-  };
-};
-
 function overFunc(el) {
   el.style.color = "red";
 }
@@ -35,3 +19,62 @@ function overFunc(el) {
 function outFunc(el) {
   el.style.color = "black";
 }
+
+// fire onload
+window.onload = function() {
+  console.log("page has finished loading!");
+
+  let message_btn = document.querySelector(".message_btn");
+  let overlay = document.querySelector("#overlay");
+
+  // clicking the button to show overlay
+  message_btn.onclick = function(event) {
+    e.preventDefault();
+    showMessage();
+  };
+
+  // clicking on the overlay to close
+  overlay.onclick = function(e) {
+    e.preventDefault();
+    hideOverlay();
+  };
+
+  // Determine if an element is in the visible viewport
+  function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || html.clientHeight) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    );
+  }
+
+  let count = 0;
+
+  function checkBottom() {
+    // console.log(count++);
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+      alert("you're at the bottom of the page");
+    }
+  }
+
+  function checkInView() {
+    let el = document.querySelector(".about_section");
+
+    if (isInViewport(el)) {
+      console.log("in view");
+      el.classList.add("showme");
+    } else {
+      el.classList.remove("showme");
+    }
+  }
+
+  // scroll event
+  window.onscroll = function(ev) {
+    // checkBottom();
+    checkInView();
+  };
+};
